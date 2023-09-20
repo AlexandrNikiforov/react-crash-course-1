6b9637a96251a69b49bb806f755e6200cc1e53c3
alexander.nikiforov <nikiforov.san.sanich@gmail.com>
2023-09-20 16:19:14 +0400

    07. Show and hide a form for adding a task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {useState} from 'react'
 import Header from "./components/Header";
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 function App() {
+
+    const [showAddTask, setShowAddTask] = useState(false)
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -30,6 +34,13 @@ function App() {
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
+    //Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...tasks, newTask])
+    }
+
     //Toggle Reminder
     const toggleReminder = (id) => {
         setTasks(
@@ -41,7 +52,9 @@ function App() {
 
     return (
         <div className="container">
-            <Header/>
+            <Header onAdd = {() => setShowAddTask
+            (!showAddTask) } showAdd={showAddTask}/>
+            {showAddTask && <AddTask onAdd={addTask}/>}
             {tasks.length > 0 ?
                 (<Tasks tasks={tasks} onDelete={deleteTask}
                 onToggle={toggleReminder}/>)
@@ -49,6 +62,5 @@ function App() {
         </div>
     );
 }
-
 
 export default App;
